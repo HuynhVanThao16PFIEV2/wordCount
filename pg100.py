61283057e5f213eb7d2a5e8ba896f1b65c12b085
HuynhVanThao16PFIEV2 <65277398+HuynhVanThao16PFIEV2@users.noreply.github.com>
2020-05-13 14:40:05 +0700

    Create pg100.py

---
+++ pg100.py
@@ -0,0 +1,16 @@
+
+import time
+
+start_time = time.time()
+file =open('C:/Users/USER/Dropbox/study/PYTHON/pg100.txt','r+')
+wordCount={}
+for word in file.read().split():
+    if word not in wordCount:
+           wordCount[word]=1
+    else:
+        wordCount[word]+=1
+for key in wordCount.keys():
+    print(key," ",wordCount[key])
+file.close()
+end_time = time.time()
+print ('total run-time: %f ms' % ((end_time - start_time) * 1000))
